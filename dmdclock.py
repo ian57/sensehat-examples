c1f03ab33d7f7d81b7a0ffb5ab950b7c9c6adf95
ian57 <yann@morere.eu>
2017-06-12 17:04:41 +0200

    update dmdclock

--- dmdclock.py
+++ dmdclock.py
@@ -2,6 +2,7 @@ from sense_hat import SenseHat
 import os
 import time
 import datetime
+import locale
 
 sense = SenseHat()
 
@@ -29,7 +30,11 @@ for nbr in range(1, 15):
 
 pacman=[]
 for nbr in range(1, 15):
-        pacman.append(sense.load_image("sprites/pacman"+str(nbr)+".png", redraw=False))
+	pacman.append(sense.load_image("sprites/pacman"+str(nbr)+".png", redraw=False))
+
+pacmanb=[]
+for nbr in range(1, 15):
+	pacmanb.append(sense.load_image("sprites/pacmanb"+str(nbr)+".png", redraw=False))
 
 def Move(sprites, direction):
         if direction:
@@ -40,9 +45,8 @@ def Move(sprites, direction):
                 sense.set_pixels(sprite)
                 time.sleep(0.1) # delays for 100 miliseconds
 
-
-
-
+# Let's set a non-US locale
+locale.setlocale(locale.LC_TIME, "fr_FR.UTF8")
 print("Setting time")
 bashCommand = "sudo service ntp stop; sudo ntpdate 193.50.119.254; sudo service ntp start"
 os.system(bashCommand)
@@ -58,19 +62,19 @@ while (True):
 	sense.show_message(now.strftime("Date : %d %B %Y "), text_colour=[0, 255, 255])
 	Move(blinky,0)
 	sense.clear()
-	sense.show_message(now.strftime("Heure : %H:%M"), text_colour=[255, 255, 0])
+	sense.show_message(now.strftime("Heure : %H:%M"), text_colour=[255, 0, 0])
 	Move(clyde,0)
 	sense.clear()
-	sense.show_message("Temperature : "+"{0:.1f}".format(temp)+" C", text_colour=[255, 255, 128])
+	sense.show_message("Temperature : "+"{0:.1f}".format(temp)+" C", text_colour=[255, 102, 0])
 	Move(pinky,0)
 	sense.clear()
 	sense.show_message("Pression : "+"{0:.0f}".format(pressure)+" mB", text_colour=[255, 0, 255])
-	Move(pacman,0)
+	Move(pacmanb,1)
 	sense.clear()
-	sense.show_message("Hygrometrie : "+"{0:.0f}".format(humidity)+"%", text_colour=[255, 0, 255])
+	sense.show_message("Hygrometrie : "+"{0:.0f}".format(humidity)+"%", text_colour=[255, 255, 0])
 	Move(deadghost,0)
 	sense.clear()
-	Move(pacman,0)
+	Move(pacmanb,1)
 	sense.clear()
 	time.sleep(0.5)
 
